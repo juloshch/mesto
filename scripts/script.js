977fd47d3fb33bb8d502a722b68b808fa0a60c27
Юлия Лощенкова <julia@MacBook-Pro-Ulia.local>
2020-10-10 23:55:05 +0300

    поправки по index.html

--- scripts/script.js
+++ scripts/script.js
@@ -2,26 +2,27 @@ let buttonOpenPopup = document.querySelector(".edit-but");
 let buttonClosePopup = document.querySelector(".popup__close-image");
 let popup = document.querySelector(".popup");
 let captionName = document.querySelector(".caption__name");
-let captionParagraph = document.querySelector(".caption__paragraph");
-let popupFieldName = document.querySelector(".popup__field-name");
-let popupFieldParagraph = document.querySelector(".popup__field-paragraph");
+let captionParagraph = document.querySelector(".captions__paragraph");
+let popupFieldName = document.querySelector(".popup__field_name");
+let popupFieldParagraph = document.querySelector(".popup__field_paragraph");
 let formElement = document.querySelector(".popup__container");
 
-let popupToggle = () => {
+let popupToggle = (evt) => {
+    evt.preventDefault();
     popup.classList.toggle("popup_is-opened");
 }
 
-let openPopup = () => {
+let openPopup = (evt) => {
     popupFieldName.value = captionName.innerText;
     popupFieldParagraph.value = captionParagraph.innerText;
-    popupToggle();
+    popupToggle(evt);
 }
 
 let formSubmitHandler = (evt) => {
     evt.preventDefault();
     captionName.textContent = popupFieldName.value;
     captionParagraph.textContent = popupFieldParagraph.value;
-    popupToggle();
+    popupToggle(evt);
 }
 
 buttonOpenPopup.addEventListener("click", openPopup);
