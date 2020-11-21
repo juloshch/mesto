9d21afaf558d069f1418c52c107744acfe8edcc7
Юлия Лощенкова <julia@MacBook-Pro-Ulia.local>
2020-11-22 00:59:57 +0300

    class card

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+import {initialCards} from './data.js';
+import {Card} from './card.js';
+
 const buttonOpenPopup = document.querySelector(".edit-but");
 const buttonClosePopup = document.querySelector(".popup__close-image");
 const editProfilePopup = document.querySelector("#edit-profile-popup");
@@ -7,9 +10,9 @@ const popupFieldName = document.querySelector(".popup__field_type_name");
 const popupFieldParagraph = document.querySelector(".popup__field_type_paragraph");
 const formProfileElement = document.querySelector("#edit-profile-container");
 const elements = document.querySelector(".elements");
-const showImagePopup = document.querySelector('.popup__image-popup');
-const largeImage = document.querySelector(".popup__image");
-const largeImageTitle = document.querySelector(".popup__image-title");
+export const showImagePopup = document.querySelector('.popup__image-popup');
+export const largeImage = document.querySelector(".popup__image");
+export const largeImageTitle = document.querySelector(".popup__image-title");
 const buttonOpenAddPlacePopup = document.querySelector(".add-but");
 const addPlacePopup = document.querySelector("#add-place-popup");
 const buttonCloseAddPlacePopup = document.querySelector("#add-place-popup-close-image");
@@ -18,14 +21,14 @@ const placeLink = document.querySelector(".popup__field_type_place-link");
 const formPlaceElement = document.querySelector("#add-place-container");
 const buttonCloseImagePopup = document.querySelector("#image-popup-close-button");
 const popups = document.querySelectorAll(".popup");
-const cardTemplate = document.querySelector("#card-template");
+const cardTemplate = document.querySelector('#card-template');
 
-const openPopup = (popup) => {
+export const openPopup = (popup) => {
     popup.classList.add("popup_is-opened");
     document.addEventListener('keydown', closeOnEscape);
 }
 
-const closePopup = (popup) => {
+export const closePopup = (popup) => {
     popup.classList.remove("popup_is-opened");
     document.removeEventListener('keydown', closeOnEscape);
 }
@@ -66,40 +69,10 @@ buttonOpenPopup.addEventListener("click", openProfilePopup);
 buttonClosePopup.addEventListener("click", () => closePopup(editProfilePopup));
 formProfileElement.addEventListener('submit', formProfileSubmit); 
 
-//карточки из массива скриптом плюс из кнопки добавить плюс удалить
-
-const handleDeleteCard = (evt) => {
-    const elementItem = evt.target.closest(".element");
-    elementItem.remove();
-};
-
-const handleLikeIcon = (evt) => {
-    evt.target.classList.toggle('element__heart_liked');
-};
-
-const handleOpenCard = (data) => {
-    largeImage.src = data.link;
-    largeImage.alt = data.name;
-    largeImageTitle.textContent = data.name;
-    openPopup(showImagePopup);
-}
-
-function createElement(data) {
-    const cardElement = cardTemplate.content.cloneNode(true);
-    const elementPic = cardElement.querySelector(".element__pic");
-    elementPic.src = data.link;
-    elementPic.alt = data.name;
-    cardElement.querySelector(".element__title").textContent = data.name;
-    const currentDeleteButton = cardElement.querySelector(".element__bin-button");
-    currentDeleteButton.addEventListener('click', handleDeleteCard);
-    const likeButton = cardElement.querySelector(".element__heart");
-    likeButton.addEventListener('click', handleLikeIcon);
-    elementPic.addEventListener('click', () => handleOpenCard(data));  
-    return cardElement;
-}
+//карточки из массива
 
 initialCards.forEach((data) => {
-    elements.append(createElement(data));
+    elements.append(new Card(data, cardTemplate).createElement());
 });
 
 // обработка попапа 'добавить место'
@@ -110,7 +83,7 @@ const formSubmitAddPlace = (evt) => {
         name: placeName.value,
         link: placeLink.value
     };
-    const newElement = createElement(newElementData);
+    const newElement = new Card(newElementData, cardTemplate).createElement();
     elements.prepend(newElement);
     closePopup(addPlacePopup);
 };
